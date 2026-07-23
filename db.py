9205f00024b61361316c43d7a28e5093692afded
renechris466-hue <renechris466@gmail.com>
2026-07-23 16:19:24 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,25 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    )
+)
+
+DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'database', 'library.db')}"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False
+)
+
+Base = declarative_base()
